5d29aa22cd1bc1b5bbafb47a491bf9c3dea668fb
lovearya05 <lovepreetarya1405@gmail.com>
2025-05-01 20:53:25 +0530

    added startup screen

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,8 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {fetchAuthState} from '../redux/slices/authSlice';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAuthState } from '../redux/slices/authSlice';
 import StackNavigation from './StackNavigation';
 import AuthNavigator from './AuthNavigator';
+import splashImg from '../assets/animation.gif';
+import { Image, View } from 'react-native';
+import FastImage from 'react-native-fast-image';
+
+
 const AppNavigator = () => {
   const dispatch = useDispatch();
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
@@ -11,7 +16,9 @@ const AppNavigator = () => {
   useEffect(() => {
     const checkAuthState = async () => {
       await dispatch(fetchAuthState());
-      setLoading(false);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1500)
     };
 
     checkAuthState();
@@ -22,7 +29,21 @@ const AppNavigator = () => {
     console.log('Auth state changed:', isAuthenticated);
   }, [isAuthenticated]);
 
-  if (loading) return null;
+  if (loading) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} >
+        <FastImage
+          source={splashImg}
+          style={{
+            height: 400,
+            width: 400
+          }}
+          resizeMode={'contain'}
+        />
+      </View>
+    )
+  }
+
 
   // Use the Redux state directly instead of local state
   return <>{isAuthenticated ? <StackNavigation /> : <AuthNavigator />}</>;
